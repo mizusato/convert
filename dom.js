45e7856f402dff34cc7c0e04ff15dd9dc62bfff2
mizusato <mizusato@protonmail.com>
2018-07-14 13:54:17 +0800

    更新 DOM Tree 中的屬性

--- dom.js
+++ dom.js
@@ -18,6 +18,7 @@ function create(data) {
   }
   
   var element = document.createElement(data.tag);
+  element.$fields = {}
 
   var reserved = ['classList', 'style', 'dataset', 'handlers', 'children'];
 
@@ -29,28 +30,46 @@ function create(data) {
     }
   }
   
-  ['style', 'dataset'].forEach(function(item) {
-    if (data[item]) {
-      for ( let I of Object.keys(data[item]) ) {
-	if (data[item][I]) {
-	  element[item][I] = data[item][I];
-	}
-      }
-    }
+  ['style', 'dataset'].forEach(function(hash_property) {
+    if (data[hash_property]) {
+      for ( let key of Object.keys(data[hash_property]) ) {
+	if (data[hash_property][key]) {
+	  if ( data[hash_property][key] instanceof field )  {
+	    let field = data[hash_property][key];
+	    let current_key = key;
+	    element.$fields[field.name] = function update(value) {
+	      element[hash_property][current_key] = value;
+	    };
+	    element[hash_property][current_key] = field.def_val;
+	  } else {
+	    element[hash_property][key] = data[hash_property][key];
+	  } // field or value
+	} // if data[hash_property] is set
+      } // for key in data[hash_property]
+    } // data of hash_property exists
   });
 
   if (data.handlers) {
-    for ( let I of Object.keys(handlers) ) {
-      element.addEventListener(I, handlers[I]);
+    for ( let event_name of Object.keys(handlers) ) {
+      element.addEventListener(event_name, handlers[event_name]);
     }
   }
 
-  for ( let I of Object.keys(data) ) {
-    if ( data[I] || typeof data[I] != 'boolean' ) {
-      if ( reserved.indexOf(I) == -1 ) {
-	element[I] = data[I];
-      }
-    }
+  for ( let property of Object.keys(data) ) {
+    if ( data[property] || typeof data[property] == 'boolean' ) {
+      if ( reserved.indexOf(property) == -1 ) {
+	if ( data[property] instanceof field ) {
+	  let field = data[property];
+	  let current_property = property;
+	  element.$fields[field.name] = function update (value) {
+	    element[current_property] = value;
+	  };
+	  element[current_property] = field.def_val;
+	} else {
+	  element[property] = data[property];
+	} // field or value
+      } // property is not children or processed property
+    } // boolean type or true value of other types
   }
 
   if (data.children) {
@@ -70,4 +89,23 @@ function map(list, f) {
     result.append(f(I));
   }
   return result;
+}
+
+
+function field(name, default_value) {
+  var field_object = {'name': name, 'def_val': default_value};
+  field_object.__proto__ = field.prototype;
+  return field_object;
+}
+
+
+function update(element, data) {
+  for ( let field_name of Object.keys(element.$fields) ) {
+    if ( data[field_name] || typeof data[field_name] == 'boolean' ) {
+      element.$fields[field_name].call(element, data[field_name]);
+    }
+  }
+  for ( let child of element.children ) {
+    update(child, data);
+  }
 }
