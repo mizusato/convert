a7060038c391bad1e3f098f11406e3ecd815ae24
mizusato <mizusato@protonmail.com>
2019-04-17 16:42:33 +0800

    require es6 in all pages

--- src/es6_test.js
+++ src/es6_test.js
@@ -1,7 +1,7 @@
 (function (window) {
     let { a } = { a: `1` }
     let b = [1,2,3]
-    let c = [...b, 4]
+    let c = [...b, (()=>4)()]
     class A {}
     let d = new A()
     window.es6_ok = true
